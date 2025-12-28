34a140fc70a24b1999012ec0ec9e9605ecb7e117
zxcvbnmlkjhuii-hue <zxcvbnmlkjhuii@gmail.com>
2025-12-28 19:08:04 +0900

    (2025.12.26)문자열

--- Program/source.c
+++ Program/source.c
@@ -15,28 +15,113 @@ void main()
 	//	A5[i] = (i+1) * 10;
 	//	printf("%d\n", A5[i]);
 	//}
-	
-	// 배열의 메모리 공간은 프로그램이 실행되는 동안 
-	// 변경할 수 없습니다.
-	
-	int array[] = {1,2,7,4,5,6,0};
-	
-	//int size = sizeof(array) / sizeof(array[0]);
+	//
+	//// 배열의 메모리 공간은 프로그램이 실행되는 동안 
+	//// 변경할 수 없습니다.
+	//
+	//int array[] = {1,2,7,4,5,6 };
+	//
+	////int size = sizeof(array) / sizeof(array[0]);
 
-	//	for (int i = 0; i < size; i++)
-	//	{
-	//		printf("%d\n", array[i]);
-	//	}
-	//	//  배열의 크기는 생략할 수 있으며, 초기하 목록에서
-	//	// 설정한 요소에 따라 배열의 크기가 결정됩니다.
-		printf("%d\n", array[1]);
-		int* pointer = &array[1];
+	////	for (int i = 0; i < size; i++)
+	////	{
+	////		printf("%d\n", array[i]);
+	////	}
+	////	//  배열의 크기는 생략할 수 있으며, 초기하 목록에서
+	////	// 설정한 요소에 따라 배열의 크기가 결정됩니다.
+	//	printf("%d\n", array[1]);
+	//	int* pointer = &array;
 
-		*pointer = +3  ;
+	//	pointer = pointer +3  ;
 
-		printf("%d", array[1]);
+	//	*pointer = 22;
+	//	 
+	//	printf("%d\n", array[1]); 
+	//	printf("%p", pointer);
+
+		// 배열의 연속직인 메모리 공간을 가지며, 배열의 이름은
+		//	배열의 시작 주소를 가리킵니다.
+
 
 #pragma endregion
+
+#pragma region 문자열
+		// 여나속적인 메모리 공간에 저장된 문자 변수의
+		// 집합입니다.
+
+		//const char* character = "world";
+		//character = "exercisc";
+
+		//printf("character가 가르키는 문자열 : %s\n", character);
+
+		//char messgc[] = "messgc";
+		//messgc[0] = 'n';
+
+		//
+
+		//printf("messgc가 가르키는 문자열 : %s\n", messgc);
+
+		//
+
+		//// 문자열의 경우 포인터를 이용하여 문자열 상수를 가리키도록 할
+		//// 수 있으며, 문자열 상수는 데이터 영역의 읽기 전용 공간에 저장
+		//// 되기 때문에 문자열의 값을 변경할 수 없습니다.
+		//
+		//// 문자열은 공백도 함께 메모리 공간에 포함하여 크기가 졀정되며,
+		//// 마지막에 문자열의 끝을 알려주는 제어 문자가 추가 됩니다.
+
+		//char shower[] = "showe\0r";
+
+		//printf("shower가 가르키는 문자열 : %s", shower);
+
+		//// 문자열의 경우 서로 연속적인 메모리 공간으로 연결되어 있지만,
+		//// 문자 배열 사이에 무효의 문자를 넣게 되면 무효의 문자까지만 출력됩니다.
+
+		//
+#pragma endregion
+#pragma region (2)차원 배열
+		// 배여릐 요소로 또 다른 배열을 가지는 배열입니다,.
+		
+		//int arrat2D[3][3] =
+		//{
+		//	{1,2,3},
+		//	{4,5,6},
+		//	{7,8,9},
+		//};
+
+		//for (int i = 0; i < 3; i++)
+		//{
+		//	
+		//	for (int j = 0; j < 3; j++)
+		//	{
+		//		printf("%d", arrat2D[i][j]);
+
+		//	}
+		//	 
+
+
+		//}
+
+		//// 2차원 배열은 행과 열로 구분되며, 앞에 있는 배열은
+		// 열을 의미하고, 뒤에 있는 배열은 행을 의미합니다.
+#pragma endregion
+
+#pragma region 아스키 코드
+		// 미국 구립 표준 협회에서 표준화한 정보 뵤환용
+		// 7 bit 부호 체계입니다.
+
+	//	char alphabet = 'A';
+
+	///*	printf("alphabet 변수의 10 진수 값 : %d", alphabet);*/
+
+	//	for (int i = 0; i < 26; i++)
+	//	{
+	//		printf("%c", 'A' + i);
+
+
+		}
+#pragma endregion
+
 
 }
 
